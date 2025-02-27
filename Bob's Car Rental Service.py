fc376e0723afe0eb775c6633566841c28a21d11c
Malek Ramadan <mintywicked@gmail.com>
2025-02-27 20:57:31 +0200

    Adding the car's model

--- Bob's Car Rental Service.py
+++ Bob's Car Rental Service.py
@@ -1,12 +1,14 @@
 class showRoom:
-    def __init__(self, brand, year, rental_ppd):
+    def __init__(self, brand, model, year, rental_ppd):
         self.brand = brand
+        self.model = model
         self.year = year
         self.rental_ppd = rental_ppd
 def display_info():
     brand = input("Enter the car brand: ")
+    model = input("Enter the car's model: ")
     year = input("Enter the year: ")
     rental_ppd = input("Enter the Rental per day: ")
-    print(f"Car: {brand}, Year: {year}, Rental price: ${rental_ppd}/day")
+    print(f"Car: {brand} {model}, Year: {year}, Rental price: ${rental_ppd}/day")
 
 display_info()
