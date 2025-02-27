a6fba9be95d541658ef837d6b80d58f06c919f99
Malek Ramadan <mintywicked@gmail.com>
2025-02-27 20:24:18 +0200

    Displaying the info

--- Bob's Car Rental Service.py
+++ Bob's Car Rental Service.py
@@ -3,3 +3,10 @@ class showRoom:
         self.brand = brand
         self.year = year
         self.rental_ppd = rental_ppd
+def display_info():
+    brand = input("Enter the car brand: ")
+    year = input("Enter the year: ")
+    rental_ppd = input("Enter the Rental per day: ")
+    print(f"Car: {brand}, Year: {year}, Rental price: ${rental_ppd}/day")
+
+display_info()
