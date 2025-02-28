72c0f3c3c4911f60e09bcd3bc1a6cb71b1253355
Malek Ramadan <mintywicked@gmail.com>
2025-02-28 19:15:48 +0200

    Adding class Bike that inherits form Vehicle and overriding the display_info() to display the engine power.

--- Bob's Car Rental Service.py
+++ Bob's Car Rental Service.py
@@ -19,5 +19,15 @@ class Car(Vehicle):
     def display_info(self):
         print(f"Brand: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seats}, Rental Price Per Day: ${self._Vehicle__rental_ppd}/day")
 
+class Bike(Vehicle):
+    def __init__(self, brand, model, year, rental_ppd, engine):
+        super().__init__(brand, model, year, rental_ppd)
+        self.engine = engine
+    
+    def display_info(self):
+        print(f"Brand: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine}cc, Rental Price Per Day: ${self._Vehicle__rental_ppd}/day")
+
 car =[Car("Toyota", "Corolla", 2015, 50, 5)]
+bike = [Bike("Yamaha", "R1", 2019, 30, 998)]
 car[0].display_info()
+bike[0].display_info()
