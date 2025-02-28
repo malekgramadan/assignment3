4ba4d41faba115795c2833c6eb2cdd9fdbf54a41
Malek <mintywicked@gmail.com>
2025-02-28 12:11:15 +0200

    Calculating the rental cost based of the days

--- Bob's Car Rental Service.py
+++ Bob's Car Rental Service.py
@@ -8,9 +8,14 @@ class Vechile:
     def display_info(self):
         print(f"Car: {self.brand} {self.model}, Year: {self.year}, Rental price: ${self.rental_ppd}/day")
 
+    def calculate_rental_cost(self, days):
+        return print(f"Rental cost for {self.brand} {self.model} {int(self.rental_ppd * days)}")
+    
 brand = input("Enter the car brand: ")
 model = input("Enter the car's model: ")
 year = input("Enter the year: ")
 rental_ppd = input("Enter the Rental per day: ")
+days = int(input("Enter the number of days you want to rent the car: "))
 car = Vechile(brand, model, year, rental_ppd)
 car.display_info()
+car.calculate_rental_cost(days)
