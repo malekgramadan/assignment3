e7fa60928343a3b22c91c83b15b50f4df89585e9
Malek <mintywicked@gmail.com>
2025-02-28 13:21:50 +0200

    Adding the engine attribute.

--- Bob's Car Rental Service.py
+++ Bob's Car Rental Service.py
@@ -1,13 +1,15 @@
 class Vehicle:
-    def __init__(self, brand, model, year, rental_ppd,seat_capacity):
+    def __init__(self, brand, model, year, rental_ppd,seat_capacity,engine):
         self.brand = brand
         self.model = model
         self.year = year
         self.rental_ppd = rental_ppd
         self.seat_capacity = seat_capacity
+        self.engine = engine
 
     def display_info(self):
-        print(f"Car: {self.brand} {self.model}, Year: {self.year},Seats: {self.seat_capacity} Rental price: ${self.rental_ppd}/day")
+        print(f"Car: {self.brand} {self.model}, Year: {self.year},Seats: {self.seat_capacity}, Rental price: ${self.rental_ppd}/day")
+        print(f"Bike: {self.brand} {self.model}, Year: {self.year},Engine: {self.engine}, Rental price: ${self.rental_ppd}/day")
 
     def calculate_rental_cost(self, days):
         return print(f"Rental cost for the {self.brand} {self.model} for {days} days: ${int(self.rental_ppd * days)}")
@@ -18,6 +20,7 @@ year = input("Enter the year: ")
 rental_ppd = input("Enter the Rental per day: ")
 seat_capacity = input("Enter the seat capacity: ")
 days = int(input("Enter the number of days you want to rent the car: "))
-car = Vehicle(brand, model, int(year), int(rental_ppd),int(seat_capacity))
+engine = input("Enter the engine type: ")
+car = Vehicle(brand, model, int(year), int(rental_ppd), int(seat_capacity), engine)
 car.display_info()
 car.calculate_rental_cost(days)
