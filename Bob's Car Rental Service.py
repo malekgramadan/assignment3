78f6b033eb20c7458ffade8b56ac43dd921cf6f9
Malek <mintywicked@gmail.com>
2025-02-28 11:55:56 +0200

    Initializing the display_info() func in the showRoom class

--- Bob's Car Rental Service.py
+++ Bob's Car Rental Service.py
@@ -4,12 +4,13 @@ class showRoom:
         self.model = model
         self.year = year
         self.rental_ppd = rental_ppd
-def display_info():
-    brand = input("Enter the car brand: ")
-    model = input("Enter the car's model: ")
-    year = input("Enter the year: ")
-    rental_ppd = input("Enter the Rental per day: ")
-    brd = showRoom(brand)
-    print(f"Car: {brd} {model}, Year: {year}, Rental price: ${rental_ppd}/day")
 
-display_info()
+    def display_info(self):
+        print(f"Car: {self.brand} {self.model}, Year: {self.year}, Rental price: ${self.rental_ppd}/day")
+
+brand = input("Enter the car brand: ")
+model = input("Enter the car's model: ")
+year = input("Enter the year: ")
+rental_ppd = input("Enter the Rental per day: ")
+brd = showRoom(brand, model, year, rental_ppd)
+brd.display_info()
