936a15196edba95b0bddc663aa404d2d493f196b
Malek <mintywicked@gmail.com>
2025-02-28 12:16:28 +0200

    Modifying the rental cost to display the total of the rental days instead of printing it multiple times / Correcting typos.

--- Bob's Car Rental Service.py
+++ Bob's Car Rental Service.py
@@ -1,4 +1,4 @@
-class Vechile:
+class Vehicle:
     def __init__(self, brand, model, year, rental_ppd):
         self.brand = brand
         self.model = model
@@ -9,13 +9,13 @@ class Vechile:
         print(f"Car: {self.brand} {self.model}, Year: {self.year}, Rental price: ${self.rental_ppd}/day")
 
     def calculate_rental_cost(self, days):
-        return print(f"Rental cost for {self.brand} {self.model} {int(self.rental_ppd * days)}")
+        return print(f"Rental cost for the {self.brand} {self.model} for {days} days: ${int(self.rental_ppd * days)}")
     
 brand = input("Enter the car brand: ")
 model = input("Enter the car's model: ")
 year = input("Enter the year: ")
 rental_ppd = input("Enter the Rental per day: ")
 days = int(input("Enter the number of days you want to rent the car: "))
-car = Vechile(brand, model, year, rental_ppd)
+car = Vehicle(brand, model, year, float(rental_ppd))
 car.display_info()
 car.calculate_rental_cost(days)
