db38e095b4f86a5974b8fc0f489f4fcec2c03a3b
Malek Ramadan <mintywicked@gmail.com>
2025-02-28 18:24:46 +0200

    Adding an If statment for display to make sure it will print for the car and bike.

--- Bob's Car Rental Service.py
+++ Bob's Car Rental Service.py
@@ -16,16 +16,6 @@ class Vehicle:
     def calculate_rental_cost(self, days):
         return print(f"Rental cost for the {self.brand} {self.model} for {days} days: ${int(self.rental_ppd * days)}")
     
-# brand = input("Enter the car brand: ")
-# model = input("Enter the car's model: ")
-# year = input("Enter the year: ")
-# rental_ppd = input("Enter the Rental per day: ")
-# seat_capacity = input("Enter the seat capacity: ")
-# days = int(input("Enter the number of days you want to rent the car: "))
-# engine = input("Enter the engine type: ")
-# car = Vehicle(brand, model, int(year), int(rental_ppd), int(seat_capacity), engine)
-# car.display_info()
-# car.calculate_rental_cost(days)
 cars = [ ]
 bikes = [ ]
 action = 0
