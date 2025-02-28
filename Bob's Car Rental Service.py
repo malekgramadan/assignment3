ca9fd04023a9e9a99f1d070fd8f0135f122d4be7
Malek <mintywicked@gmail.com>
2025-02-28 14:46:32 +0200

    Adding a while loop

--- Bob's Car Rental Service.py
+++ Bob's Car Rental Service.py
@@ -14,13 +14,29 @@ class Vehicle:
     def calculate_rental_cost(self, days):
         return print(f"Rental cost for the {self.brand} {self.model} for {days} days: ${int(self.rental_ppd * days)}")
     
-brand = input("Enter the car brand: ")
-model = input("Enter the car's model: ")
-year = input("Enter the year: ")
-rental_ppd = input("Enter the Rental per day: ")
-seat_capacity = input("Enter the seat capacity: ")
-days = int(input("Enter the number of days you want to rent the car: "))
-engine = input("Enter the engine type: ")
-car = Vehicle(brand, model, int(year), int(rental_ppd), int(seat_capacity), engine)
-car.display_info()
-car.calculate_rental_cost(days)
+# brand = input("Enter the car brand: ")
+# model = input("Enter the car's model: ")
+# year = input("Enter the year: ")
+# rental_ppd = input("Enter the Rental per day: ")
+# seat_capacity = input("Enter the seat capacity: ")
+# days = int(input("Enter the number of days you want to rent the car: "))
+# engine = input("Enter the engine type: ")
+# car = Vehicle(brand, model, int(year), int(rental_ppd), int(seat_capacity), engine)
+# car.display_info()
+# car.calculate_rental_cost(days)
+cars = [ ]
+bikes = [ ]
+action = 0
+while action != 3:
+    print("1. Add a Vehicle")
+    print("2. Display all Vehicles")
+    print("3. Calculate rental cost")
+    print("4. Exit")
+    action = int(input("Enter your choice: "))
+    if action == 1:
+        vehicletype = input("Enter the type of vehicle (Car/Bike): ")
+        brand = input("Enter the vehicle brand: ")
+        model = input("Enter the vehicle model: ")
+        year = input("Enter the year: ")
+        rental_ppd = input("Enter the Rental per day: ")
+        
