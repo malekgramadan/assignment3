4bce72e5e4bb92e22419609fe178ae90d7a900e1
Malek Ramadan <mintywicked@gmail.com>
2025-03-01 20:45:38 +0200

    Adding two empty lists with a while loop to get the use to take input.

--- Bob's Car Rental Service.py
+++ Bob's Car Rental Service.py
@@ -36,13 +36,25 @@ class Bike(Vehicle):
     def display_info(self):
         print(f"Brand: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine}cc, Rental Price Per Day: ${self.get_rental_ppd()}/day")
 
-car =[Car("Toyota", "Corolla", 2015, 50, 5)]
-bike = [Bike("Yamaha", "R1", 2019, 30, 998)]
-car[0].display_info()
-bike[0].display_info()
-car[0].calculate_rental_cost(3)
-bike[0].calculate_rental_cost(4)
-car[0].set_rental_ppd(60)
-bike[0].set_rental_ppd(40)
-car[0].display_info()
-bike[0].display_info()
+# car =[Car("Toyota", "Corolla", 2015, 50, 5)]
+# bike = [Bike("Yamaha", "R1", 2019, 30, 998)]
+# car[0].display_info()
+# bike[0].display_info()
+# car[0].calculate_rental_cost(3)
+# bike[0].calculate_rental_cost(4)
+# car[0].set_rental_ppd(60)
+# bike[0].set_rental_ppd(40)
+# car[0].display_info()
+# bike[0].display_info()
+
+car =[]
+bike = []
+action = 0
+
+while action != 5:
+    print("1. Add a Car")
+    print("2. Add a Bike")
+    print("3. Calculate Rental Cost")
+    print("4. Set Rental Price")
+    print("5. Exit")
+    action = int(input("Enter your choice: "))
